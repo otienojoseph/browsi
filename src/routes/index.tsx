a912de731f6a98b50b5a40ae376eb4e30a229f2e
OptimusJc <optimus.prime.jc@gmail.com>
2025-07-14 21:17:56 +0300

    add routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import Home from "@/pages/home";
+import About from "@/pages/about";
+import Services from "@/pages/services";
+import Case from "@/pages/case-studies";
+import Contact from "@/pages/contact";
+
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/services", element: <Services /> },
+  { path: "/case-studies", element: <Case /> },
+  { path: "/contact", element: <Contact /> },
+];
+
+export default routes;
