f77cee44b4a93a7d04bd90d2407e7c6af1d4513f
OptimusJc <optimus.prime.jc@gmail.com>
2025-07-14 21:17:24 +0300

    add relume config to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// Tailwind.config.js
+module.exports = {
+  content: [
+    "./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  presets: [require("@relume_io/relume-tailwind")],
+};
